e54549c034b291b73660e8c566bfae11b17b70d3
shib7 <shib714@gmail.com>
2025-07-01 21:59:23 -0400

    final content projection app

--- src/app/components/content-projection/weather-content/weather-content.ts
+++ src/app/components/content-projection/weather-content/weather-content.ts
@@ -1,13 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { MatIconModule } from '@angular/material/icon';
 import { interval } from 'rxjs';
 
 @Component({
   selector: 'weather-content',
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule],
   template: `
-    <div class="sky-condition"> Sky Condition: ☀️</div>
+    <div class="sky-condition"> Sky Condition: 
+      <mat-icon aria-hidden="false" aria-label="sunny icon" fontIcon="sunny"></mat-icon>
+    </div>
     <div class="temperature">Temperature: {{temperature}}°C</div>
     <div class="temperature">Last Updated At: : {{lastUpdateAt | date}}</div>
   `
