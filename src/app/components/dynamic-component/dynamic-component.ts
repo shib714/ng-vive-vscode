11f1bd0cc805f5ef1b2def096fa31361c495b9a4
shib7 <shib714@gmail.com>
2025-07-19 18:32:52 -0400

    finished input bindings without breaking anything

--- src/app/components/dynamic-component/dynamic-component.ts
+++ src/app/components/dynamic-component/dynamic-component.ts
@@ -19,21 +19,47 @@ export class DynamicComponent {
     #componentRef?: ComponentRef<Widget>;
 
     constructor() {
-        effect(() => {
-            const isCompactMode = this.compactMode();
-            this.#componentRef?.setInput('collapsed', isCompactMode)
-        })
+    //     effect(() => {
+    //         const isCompactMode = this.compactMode();
+    //         this.#componentRef?.setInput('collapsed', isCompactMode)
+    //     })
     }
 
     createComponent() {
         this.vcr()?.clear();  
-        //whenever user clicks the  'Create Component' button, we dynamically create the Widget component     
-        this.#componentRef = this.vcr()?.createComponent(Widget);
+        //whenever user clicks the  'Create Component' button, we dynamically create the Widget component 
+        
+        //REFACTORING Start
+        
+        //Few disadvantages of this code:
+        //a. This code isn't reactive; it is imperative and it would be nice to have something more declerative and reactive
+        //b. Handling of two-way data binding is very cumbersome
+        //c. There is no unified style to work with dynamic component inputs and outputs
+        //d. The current output API requires us to access the class output property 
+        //   directly which doesn't respect their output aliasing and it is different from how it works in component templates.
+
+        //In Angular 20, we got a new property called 'bindingd' in the create component config, where we can difine input, output, and two-way bindingd.
+        //For example, to create an input binding,  we can use a function called input binding (imported from Angular Core.)
+        // this.#componentRef = this.vcr()?.createComponent(Widget); -old version
+
+        //input binding also tracks changes inside the compact mode signal and reactively update the value of the collapsed input; 
+        // so we can remove the effect () inside the constructor.
+
+        //This changes concludes input binding migration without breaking anything.
+        this.#componentRef = this.vcr()?.createComponent(Widget, {
+            bindings: [
+                inputBinding('title', () => 'Weather Condition'),
+                inputBinding('description', () => "Today's weather conditions in Ottawa."),
+                inputBinding('collapsed', this.compactMode)
+            ]
+        }); 
 
         //once the Widget component is created, we set the required inputes 
-        this.#componentRef?.setInput('title', 'Weather Condition');
-        this.#componentRef?.setInput('description', 'Current weather conditions in your area.');
-        this.#componentRef?.setInput('collapsed', this.compactMode());
+
+        //With the above change, we can remove/commented the below two lines
+        //this.#componentRef?.setInput('title', 'Weather Condition'); 
+        //this.#componentRef?.setInput('description', 'Current weather conditions in your area.');
+        //this.#componentRef?.setInput('collapsed', this.compactMode());
 
         //and subscribe to the closed outputes
         this.#componentRef?.instance.collapsed
@@ -48,12 +74,7 @@ export class DynamicComponent {
         });   
         console.log('Widget Component Created...');   
 
-        //Few disadvantages of this code:
-        //a. This code isn't reactive; it is imperative and it would be nice to have something more declerative and reactive
-        //b. Handling of two-way data binding is very cumbersome
-        //c. There is no unified style to work with dynamic component inputs and outputs
-        //d. The current output API requires us to access the class output property 
-        //   directly which doesn't respect their output aliasing and it is different from how it works in component templates.
+
 
     }
     
